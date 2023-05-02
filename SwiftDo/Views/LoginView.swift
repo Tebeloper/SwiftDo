8b03c5a0bb5d95cc8f113a9e7102b18c259b553a
tebeloper <devoiderc82@gmail.com>
2023-05-02 19:45:54 +0300

    create LoginView

--- SwiftDo/Views/LoginView.swift
+++ SwiftDo/Views/LoginView.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var isSecure: Bool = true
+    @State private var email: String = ""
+    @State private var password: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                //Header
+                HeaderView()
+                
+                //Login Form
+                Form {
+                    TextField("Email...", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    HStack {
+                        if isSecure {
+                            SecureField("Password...", text: $password)
+                                .textFieldStyle(DefaultTextFieldStyle())
+                        } else {
+                            TextField("Password...", text: $password)
+                                .textFieldStyle(DefaultTextFieldStyle())
+                        }
+                        
+                        Button {
+                            isSecure.toggle()
+                        } label: {
+                            Image(systemName: isSecure ? "eye.slash" : "eye")
+                                .foregroundColor(.pink)
+                        }
+                    }
+                    
+                    
+                    Button {
+                        // Attempt log in...
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.blue)
+                            
+                            Text("Log In")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    }
+                    .padding()
+                    
+                }
+                
+                //Create Account
+                VStack{
+                    Text("New around here?")
+                    
+                    NavigationLink("Create an Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
     }
 }
 
