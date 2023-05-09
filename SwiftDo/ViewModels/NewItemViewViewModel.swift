56d24e2f41525f04f7e372c1987414aadcea9686
tebeloper <devoiderc82@icloud.com>
2023-05-09 16:10:17 +0300

    create todoItem model

--- SwiftDo/ViewModels/NewItemViewViewModel.swift
+++ SwiftDo/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Dimitrios Gkarlemos on 01/05/2023.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -16,7 +18,26 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
+        // Get current userID
+        guard let userId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Create a model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false)
+        
+        // Save the model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
